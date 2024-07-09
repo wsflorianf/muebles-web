39a46159e88646387cd41d1df6784eab41b92186
Santiago Florian <floranwsfrench@gmail.com>
2024-07-09 14:29:40 -0500

    Fuente de icono de Whatsapp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
         <footer>
-            s
+            <a target="_blank" href="https://icons8.com/icon/16713/whatsapp">WhatsApp</a> icono de <a target="_blank" href="https://icons8.com">Icons8</a>
         </footer>
     )
 }
