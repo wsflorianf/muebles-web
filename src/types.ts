d15afff3ab40240eff8c5d403f02ade2245407c5
Santiago Florian <floranwsfrench@gmail.com>
2024-07-12 21:54:37 -0500

    Tipos de producto y de la información de productos

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,14 @@ export type MySxProps = {
     [key: string]: SxProps<Theme>;
   };
   
+export interface Product {
+  id: number
+  name: string
+  description: string
+  images: string[]
+}
+
+export interface ProductsData{
+  path: string
+  products: Product[]
+}
