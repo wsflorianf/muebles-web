a10b795ed27a86080408659631de08d265e32d62
Santiago Florian <floranwsfrench@gmail.com>
2024-07-12 21:58:21 -0500

    Se carga información de JSON para la galería de habitación

--- src/pages/Bedroom.tsx
+++ src/pages/Bedroom.tsx
@@ -1,11 +1,12 @@
+import { useEffect } from "react";
 import Gallery from "../components/Gallery";
+import data from '../assets/bedroom-data.json'
 
 export default function Bedroom(){
+
     return(
         <div className="section">
-            <Gallery title="Habitaciones">
-                
-            </Gallery>
+            <Gallery title="Habitaciones" data={data}/>
         </div>
     )
 }
