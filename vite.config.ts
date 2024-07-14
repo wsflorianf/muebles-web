79cb656406cc42319ea643df286d762f9fa92593
Santiago Florian <floranwsfrench@gmail.com>
2024-07-14 17:05:27 -0500

    Usar pnpm para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://wsflorianf.github.io/muebles-web'
+  base: '/muebles-web/'
 })
