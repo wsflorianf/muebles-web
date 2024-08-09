65c8d3b84b16aab3ec35f75f264f0c5d39210dc5
Santiago Florian <floranwsfrench@gmail.com>
2024-08-09 13:02:27 -0500

    ToolTip añadido al botón

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -1,11 +1,17 @@
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip } from '@mui/material'
 import whatsappIcon from '../assets/whatsapp-icon.svg'
 
-export default function WhatsappButton(){
-    const url = 'https://api.whatsapp.com/send?phone=573028603069&text=Buen%20d%C3%ADa%2C%20me%20gustar%C3%ADa%20mandar%20a%20realizar%20un%20trabajo.%20%F0%9F%98%81'
-    return (
-        <IconButton onClick={()=>window.open(url)} id="whatsapp-icon">
-            <img  src={whatsappIcon} alt="icono de Whatsapp" />
-        </IconButton>
-    )
+export default function WhatsappButton() {
+  const url =
+    'https://api.whatsapp.com/send?phone=573028603069&text=Buen%20d%C3%ADa%2C%20me%20gustar%C3%ADa%20mandar%20a%20realizar%20un%20trabajo.%20%F0%9F%98%81'
+
+    // componentsProps={{tooltip: {sx: {backgroundColor: 'red'}}}}
+    
+  return (
+    <Tooltip title='Contáctame' placement='left' arrow>
+      <IconButton onClick={() => window.open(url)} id='whatsapp-icon'>
+        <img src={whatsappIcon} alt='icono de Whatsapp' />
+      </IconButton>
+    </Tooltip>
+  )
 }
